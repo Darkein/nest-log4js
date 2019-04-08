8ebae3707b8e58eb1adb4b3d6f2898e85c4926e6
Darkein <pierre.fiorelli@gmail.com>
2019-04-08 14:39:54 +0700

    Update log4js.interceptor.abstract.ts

--- src/log4js.interceptor.abstract.ts
+++ src/log4js.interceptor.abstract.ts
@@ -1,4 +1,4 @@
-import { NestInterceptor, ExecutionContext,CallHandler } from '@nestjs/common';
+import { NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { Logger } from 'log4js';
